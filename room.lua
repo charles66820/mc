255d520d5341f3913f4dbfecfeccceb88c328b4f
charles66820 <charles.goedefroit@gmail.com>
2021-07-03 17:17:46 +0200

    add ars to room and fix ceil

--- room.lua
+++ room.lua
@@ -6,77 +6,118 @@ local width = 20
 local length = 11
 local height = 6
 local haveCeil = true
-local args = { ... }
+local args = {...}
 
-if #args >= 1 then
+if #args == 1 then
   width = tonumber(args[1])
+elseif #args == 2 then
+  width = tonumber(args[1])
+  length = tonumber(args[2])
+elseif #args == 3 then
+  width = tonumber(args[1])
+  length = tonumber(args[2])
+  height = tonumber(args[3])
+elseif #args == 4 then
+  width = tonumber(args[1])
+  length = tonumber(args[2])
+  height = tonumber(args[3])
+  if args[4] == "true" then
+    haveCeil = true
+  else
+    haveCeil = false
+  end
+elseif #args > 4 then
+  print("Usage: ", args[0], " <width> <length> <height> <haveCeil>")
+  os.exit()
+end
+
+if width <= 0 then
+  print("Longeur :")
+  width = tonumber(read())
+end
+
+if length <= 0 then
+  print("Largeur :")
+  length = tonumber(read())
+end
+
+if height <= 0 then
+  print("Hauteur :")
+  height = tonumber(read())
 end
 
 -- functions
 function digCeil()
-  tfun.digUpAndUp(1)
-  if height % 2 == 0 then tfun.turnAround() end
+  if height >= 3 then
+    tfun.digUpAndUp(height - 2)
+  end
+  if height % 2 == 0 then
+    tfun.turnAround()
+  end
   tfun.digAndForward(2)
   tfun.digUpAndUp(1)
   turtle.turnLeft()
-  tfun.digAndForward(width-2)
-  turtle.turnRight()
-  tfun.digAndForward(1)
-  turtle.turnRight()
-  tfun.digAndForward(width-2)
-  turtle.turnLeft()
-  while turtle.detectUp() do
-    turtle.digUp()
-  end
-  tfun.digAndForward(1)
-  turtle.turnLeft()
-  for i = 1, width-2 do
+  tfun.digAndForward(width - 1)
+  tfun.turnDigAndForwardRight()
+  tfun.digAndForward(width - 1)
+  tfun.turnDigAndForwardLeft()
+  -- digUpAndForward
+  for i = 1, width - 1 do
     while turtle.detectUp() do
       turtle.digUp()
     end
     tfun.digAndForward(1)
   end
-  turtle.turnRight()
   while turtle.detectUp() do
     turtle.digUp()
   end
-  tfun.digAndForward(1)
-  turtle.turnRight()
-  for i = 1, width-2 do
+
+  tfun.turnDigAndForwardRight()
+  -- digUpAndForward
+  for i = 1, width - 1 do
     while turtle.detectUp() do
       turtle.digUp()
     end
     tfun.digAndForward(1)
   end
-  turtle.turnLeft()
   while turtle.detectUp() do
     turtle.digUp()
   end
-  tfun.digAndForward(1)
-  turtle.turnLeft()
-  for i = 1, width-2 do
+
+  tfun.turnDigAndForwardLeft()
+  -- digUpAndForward
+  for i = 1, width - 1 do
     while turtle.detectUp() do
       turtle.digUp()
     end
     tfun.digAndForward(1)
   end
-  turtle.turnRight()
-  tfun.digAndForward(1)
-  turtle.turnRight()
-  tfun.digAndForward(width-2)
-  turtle.turnLeft()
-  tfun.digAndForward(1)
-  turtle.turnLeft()
-  tfun.digAndForward(width-2)
+  while turtle.detectUp() do
+    turtle.digUp()
+  end
+
+  tfun.turnDigAndForwardRight()
+  tfun.digAndForward(width - 1)
+  tfun.turnDigAndForwardLeft()
+  tfun.digAndForward(width - 1)
 end
 
 function mainLoop()
-  shell.run("rect " .. width .. length .. height .. "true")
-  if haveCeil then digCeil() end
+  shell.run("rect " .. width .. " " .. length .. " " .. height .. " true")
+  if haveCeil then
+    digCeil()
+  end
 end
 
 function clean()
-  -- TODO
+  if haveCeil then
+    tfun.turnAround()
+    tfun.digAndForward(width - 1)
+    turtle.turnRight()
+    tfun.digAndForward(3) -- NOTE: make relative ?
+    turtle.turnRight()
+    tfun.digDownAndDown(height - 2)
+  end
 end
 
 function start()
