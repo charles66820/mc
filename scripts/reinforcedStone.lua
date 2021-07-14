fe151a1c5f0d6e9484d740e2411d0f943e29cd1e
charles66820 <charles.goedefroit@gmail.com>
2021-07-14 17:17:08 +0200

    reinforced stone farm script

---
+++ scripts/reinforcedStone.lua
@@ -0,0 +1,26 @@
+-- Farm ash
+local tfun = turtleLib
+-- Args and vars def
+local args = {...}
+
+-- functions
+function start()
+  while true do
+    turtle.turnRight()
+    tfun.dropAllItems({"ic2:resource"}, "front")
+    turtle.turnRight()
+    tfun.suckItem("front", "ic2:scaffold", 1)
+    tfun.turnAround()
+    tfun.dropAllItems({"ic2:foam_sprayer"}, "down")
+    tfun.suckItem("up", "ic2:foam_sprayer", 1)
+    tfun.place("ic2:scaffold", "front")
+    tfun.place("ic2:foam_sprayer", "front")
+    turtle.turnLeft()
+    tfun.place("minecraft:redstone_torch", "front")
+    turtle.dig()
+    turtle.turnRight()
+    turtle.dig()
+  end
+end
+
+start()
