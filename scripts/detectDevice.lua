0b0e86f60b05007c05be90790adfbc91d0cd04dd
charles66820 <charles.goedefroit@gmail.com>
2021-07-29 12:13:00 +0200

    detectall sides

--- scripts/detectDevice.lua
+++ scripts/detectDevice.lua
@@ -1,43 +1,25 @@
 -- Detect machine
 local cfun = computerLib
 -- Args and vars def
-local side = nil
 local args = {...}
 
-if #args == 1 then
-  side = args[1]
-elseif #args > 1 then
-  print("Usage: ", args[0], " <side>")
-  return 128
-end
-
-while side == nil or not cfun.hasValue({"front", "back", "left", "right", "top", "down"}, side) do
-  print("Side :")
-  side = read()
-end
-
-if side == "front" then
-  side = ""
-end
-
 -- Main
-local periList = peripheral.getNames()
+local sides = peripheral.getNames()
 
-for i = 1, #periList do
-	print("I have a "..peripheral.getType(periList[i]).." attached as \""..periList[i].."\".")
-end
-
-os.sleep(60)
-
-if not peripheral.isPresent(side) then
+if #sides == 0 then
   cfun.printProcess("Device is not present")
   return 1
 end
 
-cfun.printProcess(peripheral.getType(side) .. " found!")
-
-for i, v in ipairs(peripheral.getMethods(side)) do
-  print(i .. ". " .. v)
+for i, side in ipairs(sides) do
+  print(peripheral.getType(side) .. " found at \"" .. side .. "\".")
+	for j, v in ipairs(peripheral.getMethods(side)) do
+		if j == 1 then
+			term.write("Methods : " .. v)
+		else
+			term.write(", " .. v)
+		end
+	end
 end
 
 while true do
