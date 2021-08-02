1f83b96ac536747e594b839299ed6b67c996f9c4
charles66820 <charles.goedefroit@gmail.com>
2021-08-02 22:50:58 +0200

    add grid

--- scripts/imgTest.lua
+++ scripts/imgTest.lua
@@ -37,14 +37,22 @@ local txt = "(" .. sWidth .. ", " .. sHeight .. ")"
 screen.setCursorPos(math.floor(sWidth / 2) - math.floor(#txt / 2), math.floor(sHeight / 2))
 screen.write(txt)
 
+for y = 1, sHeight, 1 do
+  for x = 1, sWidth, 1 do
+    if y % 2 == 0 and x % 2 == 0 then
+      paintutils.drawImage(j, y, colors.gray)
+    end
+  end
+end
+
 for i = 1, sWidth, 5 do
   screen.setCursorPos(i, 1)
-  screen.write(i)
+  screen.write(math.round(math.floor(i + 0.5)))
 end
 
 for i = 1, sHeight, 5 do
   screen.setCursorPos(1, i)
-  screen.write(i)
+  screen.write(math.round(math.floor(i + 0.5)))
 end
 
 screen.setTextColor(saveColor)
@@ -53,7 +61,7 @@ term.setCursorPos(saveX, saveY)
 -- draw img
 term.redirect(screen)
 
-screen.setTextScale(0.5)
+screen.setTextScale(1)
 
 if iconName ~= nil and iconName ~= "" and fs.exist(iconName) then
   local image = paintutils.loadImage(iconName)
