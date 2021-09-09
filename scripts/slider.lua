3b5f29c5eecd54324e37b350031ba897ca19bf50
charles66820 <charles.goedefroit@gmail.com>
2021-09-10 00:14:56 +0200

    add windows to slider

--- scripts/slider.lua
+++ scripts/slider.lua
@@ -13,9 +13,26 @@ end
 
 screen.setTextScale(0.5)
 
+local windows = {}
+local sWidth, sHeight = screen.getSize()
+
+-- load image in window
+for i, iconName in ipairs(images) do
+  sfun.drawIcon(iconName, 1, 1, screen)
+  local win = window.create(screen, x, y, sWidth, sHeight, false)
+  sfun.drawIcon(iconName, 1, 1, win)
+  table.insert(windows, win)
+  os.sleep(10)
+end
+
 while true do
-  for i, iconName in ipairs(images) do
-    sfun.drawIcon(iconName, 1, 1, screen)
-    os.sleep(10)
+  local lastWin = nil
+  for i, win in ipairs(windows) do
+    if lastWin ~= nil then
+      lastWin.setVisible(false)
+    end
+    win.setVisible(true)
+    win.redraw()
+    lastWin = win
   end
 end
